211959328cabe553513adb594b50754120a3d88e
sahana-gudi <sonugudi472@gmail.com>
2025-10-15 11:12:06 +0530

    save

--- calculation.py
+++ calculation.py
@@ -2,5 +2,9 @@ a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 sum=a+b
 sub=a-b
+mul=a*b
+div=a/b
 print("Addition:", a + b)
 print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b if b != 0 else "Error: Division by zero")
